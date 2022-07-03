44dcaa42982087fd73760bd210365a14c752308c
Marvin Siri <marvinsiri123@gmail.com>
2022-07-02 21:12:21 -0400

    adding card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Fragment } from "react";
+import Card from "./components/Card";
 import Header from "./layout/Header";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <Card />
     </Fragment>
   );
 }
