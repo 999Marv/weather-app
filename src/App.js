59cf5f8b2b32edc248b706d680eda7149eb0f920
Marvin Siri <marvinsiri123@gmail.com>
2022-07-07 13:57:26 -0400

    api function added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import Header from "./layout/Header";
 import Items from "./components/Items";
 
 function App() {
+  async function fetchWeather(city) {
+    try {
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=03c1d3c5a4e6b179f8fc88ec3235800a`
+      );
+
+      const data = await res.json();
+      console.log(data.main.temp);
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+
   return (
     <Fragment>
       <Header />
